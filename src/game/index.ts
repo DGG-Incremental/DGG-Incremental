dc268ddcd68bb3621a3b44b6b03bb9a3773698d7
citizen-thayne <>
2020-08-11 20:00:46 -0500

    tsconfig tweaks

---
+++ src/game/index.ts
@@ -0,0 +1,5 @@
+export { createGame } from './game'
+export type { Game } from './types'
+export * from './resource'
+export * from './actions'
+export * from './fabricators'
--- src/game/validations/index.ts
+++ src/game/validations/index.ts
@@ -1,24 +1,26 @@
-import orderBy from "lodash/orderBy"
-import some from "lodash/some"
-
-import { Game, GameState } from "../resource"
-
-export const exceedsRateLimit = ( game: Game, rateLimit = 15000 ) => {
-  const timeline = orderBy( game.state.actions, ["timestamp"] )
-  return some( timeline, ( action, i, actions ) => {
-    const start = action.timestamp
-    const end = new Date( start.getTime() + 1000 )
-    let k: number
-    for ( k = i + 1; k < actions.length; k++ ) {
-      if ( actions[k].timestamp.getTime() > end.getTime() ) {
-        return rateLimit <= k - i - 1
-      }
-    }
-    return rateLimit <= k - i - 1
-  } )
-}
+// import orderBy from "lodash/orderBy"
+// import some from "lodash/some"
+// import { Game } from "../types"
 
 
+// export const exceedsRateLimit = (game: Game, rateLimit = 15000) => {
+//   const timeline = orderBy(game.state.actions, ["timestamp"])
+//   return some(timeline, (action, i, actions) => {
+//     const start = action.timestamp
+//     const end = new Date(start.getTime() + 1000)
+//     let k: number
+//     for (k = i + 1; k < actions.length; k++) {
+//       if (actions[k].timestamp.getTime() > end.getTime()) {
+//         return rateLimit <= k - i - 1
+//       }
+//     }
+//     return rateLimit <= k - i - 1
+//   })
+// }
 
-export const validateGameState = ( gameState: GameState ) => {
-}
+
+
+// export const validateGameState = (gameState: GameState) => {
+// }
+
+export default 'placeholder'
